accb75835ccbe2f7ffe2436a70e21bb28a7107c6
Leandro <trairicarvalho@gmail.com>
2026-03-04 15:39:58 -0300

    first: cria projeto spring boot

---
+++ src/main/java/com/leandro/library_manager_JPA/LibraryManagerJpaApplication.java
@@ -0,0 +1,13 @@
+package com.leandro.library_manager_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagerJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandro/library_manager_JPA/LibraryManagerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.library_manager_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
